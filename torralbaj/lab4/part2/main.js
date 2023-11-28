ff7d166335431968de50a0bac7857c9769a73607
jadentorralbadc <jaden.torralba@dcmail.ca>
2023-11-28 15:17:40 -0500

    The rest

--- torralbaj/lab4/part2/main.js
+++ torralbaj/lab4/part2/main.js
@@ -8,13 +8,13 @@ const overlay = document.querySelector('.overlay');
 const images = ['pic1.jpg','pic2.jpg','pic3.jpg','pic4.jpg','pic5.jpg']
 
 /* Declaring the alternative text for each image file */
-
+const altTexts = ['pic1.jpg'='Closeup of an eyeball', 'pic2'='random quartz', 'pic3'='violets', 'pic4'='greek art', 'pic5'='butterfly']
 /* Looping through images */
 
-
+//for loop
 const newImage = document.createElement('img');
-newImage.setAttribute('src', xxx);
-newImage.setAttribute('alt', xxx);
+newImage.setAttribute('src', images[num]);
+newImage.setAttribute('alt', altTexts[images[num]]);
 thumbBar.appendChild(newImage);
 
 /* Wiring up the Darken/Lighten button */
